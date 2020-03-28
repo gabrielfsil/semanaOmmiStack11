c474ab0c282bd107ce17dd3af7a8c0626b240bc3
Gabriel Fernandes <gabrielfsil264@gmail.com>
2020-03-28 00:14:28 -0300

    Construção do frontend

---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Logon from './pages/Logon/index'
+import Register from './pages/Register/index'
+import Profile from './pages/Profile/index'
+import NewIncidents from './pages/NewIncidents/index'
+
+
+
+export default props => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncidents} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
